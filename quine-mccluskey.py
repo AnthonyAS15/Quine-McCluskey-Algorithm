fab30938f58d70e85ed3e9ff3b1c1bcc01e4a5b5
AnthonyAS15 <aanthonyartaviasalazar@gmail.com>
2022-08-22 18:35:13 -0600

    Creación de la función min_binario

---
+++ quine-mccluskey.py
@@ -0,0 +1,18 @@
+#Bibliotecas
+
+
+
+########################################################
+
+#Funciones
+
+def min_binario(minterminos):
+    min_binarios = []
+    for min in minterminos:
+        mintermino = format(min, "b")
+        min_binarios.append(mintermino)
+    return min_binarios
+
+########################################################
+
+#Main
