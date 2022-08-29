ea962f6e9351ab720fded2f9e4c30f45f25e8535
Justinsegurar <fabianjs2013@estudiantec.cr>
2022-08-28 21:50:08 -0600

    Miniterminos_1s_terminado

--- quine-mccluskey.py
+++ quine-mccluskey.py
@@ -13,18 +13,31 @@ def min_binario(minterminos):
         min_binarios.append(mintermino)
     return min_binarios
 
-def list_strings_minterms (min_binarios):   
+#Función para agrupar miniterminos 1s 
+def Agrupar_min_1s (min_binario): #recibe una lista implementada por min_binarios e itera por cada elemento de la lista 
+    Lista_de_lista = []
+    for num in min_binario:
+        contador = 0 #asignamos un contador en 0 
+        for buscar_1_min in num:
+            if buscar_1_min == "1": #verifica si en el la lista de los binarios se encuentran 1s 
+                contador+=1
+        Lista_de_lista.append(contador)
+    return Lista_de_lista
 
-    return list_strings_minterms
 
+def main():
+    num_binarios = min_binario([1, 3, 5, 8])
+    print ("Función def min_binario(minterminos): =")
+    for num in num_binarios:
+        print(num)
+    num_binarios.sort()
+    Agrupacion_de_1s = Agrupar_min_1s(num_binarios)
+    print ("Función def Agrupar_min_1s (min_binario): ")
+    for i in range(len(num_binarios)):
+        print(num_binarios[i],"|", Agrupacion_de_1s[i])
 
-
-
-    
 ########################################################
 
 #Main
-num_binarios = min_binario([1,3,5,8])
-
-for num in num_binarios:
-    print(num)
+main()
+    
