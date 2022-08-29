9d62cb3ac757d830069d51069518af568947f309
AnthonyAS15 <anthonyartaviasalazar@gmail.com>
2022-08-28 22:38:44 -0600

    Prueba en el main con nuevos minterminos

--- quine-mccluskey.py
+++ quine-mccluskey.py
@@ -6,27 +6,27 @@
 
 #Funciones
 
+#Función para transformar los minterminos a su forma binaria
 def min_binario(minterminos):
     min_binarios = []
     for min in minterminos:
-        mintermino = format(min, "b")
+        mintermino = format(min, "b") #Transforma un número entero a uno binario. int -> string
         min_binarios.append(mintermino)
-    return min_binarios
+    return min_binarios #Retorna una lista con los minterminos en su forma binaria.
 
 #Función para agrupar miniterminos 1s 
-def Agrupar_min_1s (min_binario): #recibe una lista implementada por min_binarios e itera por cada elemento de la lista 
+def Agrupar_min_1s (min_binario): #Recibe una lista implementada por min_binarios e itera por cada elemento de la lista 
     Lista_de_lista = []
     for num in min_binario:
-        contador = 0 #asignamos un contador en 0 
+        contador = 0 #Se asigna un contador en 0 
         for buscar_1_min in num:
-            if buscar_1_min == "1": #verifica si en el la lista de los binarios se encuentran 1s 
+            if buscar_1_min == "1": #Verifica si en el la lista de los binarios se encuentran 1s 
                 contador+=1
         Lista_de_lista.append(contador)
     return Lista_de_lista
 
-
 def main():
-    num_binarios = min_binario([1, 3, 5, 8])
+    num_binarios = min_binario([1, 2, 3, 4, 11, 12, 13, 14])
     print ("Función def min_binario(minterminos): =")
     for num in num_binarios:
         print(num)
