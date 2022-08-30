57afd9f7690dd53cac9219502b608015e0d6c07a
AnthonyAS15 <anthonyartaviasalazar@gmail.com>
2022-08-29 21:54:42 -0600

    Encontrar implicantes primos terminada

--- quine-mccluskey.py
+++ quine-mccluskey.py
@@ -10,6 +10,7 @@
 def min_binario(minterminos):
     min_binarios = []
     cantidad_bits = len(format(minterminos[-1], "b")) #Mayor cantidad de bits de los minterminos disponibles.
+
     for min in minterminos:
         mintermino = format(min, "b") #Transforma un número entero a uno binario. int -> string.
         mintermino = mintermino.zfill(cantidad_bits)
@@ -41,17 +42,36 @@ def Agrupar_min_1s (min_binario): #Recibe una lista implementada por min_binario
                     break
     return Lista_de_lista
 
+#Función para eliminar el indicador al inicio de las agrupaciones de 1s
+def eliminar_indicador(agrupacion_de_1s):
+    cant_grupos = len(agrupacion_de_1s) #Cantidad de agrupaciones de 1s que tiene la lista de listas.
+
+    for num in range(cant_grupos):
+        agrupacion_de_1s[num].pop(0)
+
+    return agrupacion_de_1s
+
+#Función para elimintar listas vacías
+def eliminar_vacias(lista_de_listas):
+    lista_sin_vacias = []
+    for lista in lista_de_listas:
+        if lista != [""]:
+            lista_sin_vacias.append(lista)
+    
+    return lista_sin_vacias
+
 #Función que compara si dos minterminos solo difieren en un bit
 def comparar(min1, min2):
     limite = 0
     cantidad_bits = len(min1)
     nuevo_min = ""
+
     for contador1 in range(cantidad_bits):
         bit1 = min1[contador1]
         bit2 = min2[contador1]
         if bit1 != bit2:
             limite +=1
-            nuevo_min += "-" #Sustituye el bit que difiere por "-"
+            nuevo_min += "X" #Sustituye el bit que difiere por "-"
         else:
             nuevo_min += bit1 
     if limite < 2:
@@ -59,43 +79,85 @@ def comparar(min1, min2):
     else:
         return False, nuevo_min
 
+#Función para comprobar si el mintermino ya ha sido agrupado con anterioridad o no.
+def min_agrupado(min):
+    for bit in min:
+        if bit == "X":
+            return True
+    return False
 
 #Función para encontrar los implicantes primos
+#Esta función va a hacer uso de la recursividad para cumplir con su propósito
 def encontrar_implicantes_primos(agrupacion_de_1s):
-    implicantes_primos = []
-    cantidad_grupos = len(agrupacion_de_1s) -1 #Cuenta la cantidad de grupos formados -1 a partir de la cantidad de unos de cada uno.
+    agrupacion_de_1s = eliminar_indicador(agrupacion_de_1s) #Eliminar el indicador de 1s al comienzo de la lista
+    agrupacion_de_1s = eliminar_vacias(agrupacion_de_1s) #Elimina las listas vacías que existan dentro de la lista.
+    implicantes = [] #Almacena los implicantes que aún no se sabe si son primos.
+    implicantes_primos = [] 
+    posibles_implicantes_primos = [] #Almacena los implicantes del último que pueden ser implicantes primos.
+    comprobacion_implicantes_primos = [] #Almacena una lista de booleanos que verifican si un mintermino no se pudo relacionar con ningún otro.
+    cantidad_grupos = len(agrupacion_de_1s)-1 #Cuenta la cantidad de grupos por cantidad de 1s excepto el ultimo.
 
-    for contador1 in range(cantidad_grupos):
-        longitud_grupo = len(agrupacion_de_1s[contador1])
-
-        for contador2 in range (1, longitud_grupo): #Se inicia en 1 el contador para no tomar en cuenta el indicador de cada grupo.
-            for min1 in agrupacion_de_1s[contador1][contador2]:
-                for min2 in agrupacion_de_1s[contador1+1][contador2]: #Se suma uno para poder tomar al grupo siguiente del actual.
-                    difiere_1bit, nuevo_min = comparar(min1, min2) #Recibe True si los minterminos solo difieren en un 1 bit.
+    if cantidad_grupos > 0: #Si la lista posee dos grupos o más.
+        for contador1 in range(cantidad_grupos):
+            for contador2, min1 in enumerate(agrupacion_de_1s[contador1]): #Se inicia en 1 el contador para no tomar en cuenta el indicador de cada grupo.
+                for contador3, min2 in enumerate(agrupacion_de_1s[contador1+1]):
+                    difiere_1bit, nuevo_min = comparar(min1, min2) #Recibe True si los minterminos solo difieren en un 1 bit y el mintermino de la comparación.
                     if difiere_1bit == True:
-                        implicantes_primos.append(nuevo_min)
-    
+                        implicantes.append(nuevo_min) #Añade el mintermino que difiere solo en 1 bit a la lista implicantes.
+                        if contador1 == cantidad_grupos -1: #Si un mintermino del último grupo difiere en un solo bit
+                            if min2 in posibles_implicantes_primos and min_agrupado(min2):#, se borra de la lista de posibles_implicantes_primos.
+                                posibles_implicantes_primos.remove(min2)
+                    else:
+                        if contador1 == cantidad_grupos -1: #Si es del último grupo
+                            if min2 not in posibles_implicantes_primos and min_agrupado(min2): #Si el mintermino no ha sido combinado y no está aún en la lista
+                                posibles_implicantes_primos.append(min2) #Se va a insertar el mintermino del último grupo en posibles_implicantes_primos
+                    comprobacion_implicantes_primos.append(difiere_1bit)
+                for cant, comprobacion in enumerate(comprobacion_implicantes_primos): #Comprobar si el elemento es un implicante primo o no.
+                    if comprobacion == True:
+                        break
+                    elif comprobacion == False and cant == len(comprobacion_implicantes_primos) -1 and min_agrupado(min1):
+                        implicantes_primos.append(min1) #Añadir el mintermino a los implicantes primos dado que no se pudo emparejar.
+
+                comprobacion_implicantes_primos = []
+        
+        grupos_implicantes = Agrupar_min_1s(implicantes)
+        implicantes_primos += encontrar_implicantes_primos(grupos_implicantes)
+        if posibles_implicantes_primos != []:
+            for implicante in posibles_implicantes_primos:
+                implicantes_primos.append(implicante)
+    elif cantidad_grupos == 0: #Si la lista solo posee un grupo.
+        for implicante in agrupacion_de_1s:
+            implicantes_primos.append(implicante)
+
     return implicantes_primos
 
 #Función para determinar los implicantes primos esenciales
-#def encontrar_implicantes_esenciales ():
+def encontrar_implicantes_esenciales ():
+    pass
 
 
 #Función que va a ejecutar el código principal del programa
 def main():
-    minterminos = [1, 2, 3, 4, 11, 12, 13, 14]
+    minterminos = [4, 8, 11, 12, 15]
     minterminos.sort()
+
     num_binarios = min_binario(minterminos)
     print ("Función def min_binario(minterminos): =")
     print(num_binarios)
     num_binarios.sort()
+
     agrupacion_de_1s = Agrupar_min_1s(num_binarios)
     agrupacion_de_1s.sort(key=lambda x: x[0])
-    print ("Función def Agrupar_min_1s (min_binario): ")
+    print ("Función def agrupar_min_1s (min_binario): ")
     print(agrupacion_de_1s)
+
     comparacion, nuevo_min = comparar("1101","1111")
-    print(comparacion)
-    print(nuevo_min)
+    print ("Función comparar(min1, min2): ")
+    print(comparacion, nuevo_min)
+
+    implicantes_primos = encontrar_implicantes_primos(agrupacion_de_1s)
+    print("Función encontrar_implicantes_primos(agrupacion_de_1s): ")
+    print(implicantes_primos)
 
 ########################################################
 
